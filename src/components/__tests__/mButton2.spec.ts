dd2312876eb8fc40dca84bc5847f27198b8a8d13
digos <diogobissolimoreno@gmail.com>
2024-06-20 22:03:05 -0300

    Initial

---
+++ src/components/__tests__/mButton2.spec.ts
@@ -0,0 +1,15 @@
+import mButton2 from '../m-button2.vue'
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+test('verificar o click do botão dois', async () => {
+  // const valueSelector = "[data-testid=stepper-value]"; -> alternativamente pode ser feito com data atributes
+  const buttonSelect = '[role="button"]';
+
+  console.log(mButton2);
+  const wrapper = mount(mButton2);
+
+  expect(wrapper.find(buttonSelect).text()).toContain("0");
+  await wrapper.find(buttonSelect).trigger("click");
+  expect(wrapper.find(buttonSelect).text()).toContain("1");
+})
---
+++ src/components/m-button2.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const increment = ref(0)
+</script>
+
+<template>
+  <div role="button" @click="increment++">
+    teste {{ increment }}
+  </div>
+</template>
+
+<style scoped>
+</style>
+
