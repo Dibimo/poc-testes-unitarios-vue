70c0ee0788e473feb314b5f067bb85344bf8c74f
digos <diogobissolimoreno@gmail.com>
2024-06-21 22:30:25 -0300

    feat: adicionando componente Input Text

---
+++ src/components/m-Input-Text.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emit = defineEmits(['change'])
+
+const inputTextValue = ref('')
+
+</script>
+
+<template>
+    <input
+      type="text"
+      role="input-text"
+      v-model="inputTextValue"
+      @input="emit('change')"
+    >
+</template>
+
+<style scoped>
+</style>
