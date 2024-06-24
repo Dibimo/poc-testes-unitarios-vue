4beaafece18d2a06bda409de517aabd87beadcb7
digos <diogobissolimoreno@gmail.com>
2024-06-24 20:50:28 -0300

    test: adicionando novo caso de teste para componente de input

--- src/components/__tests__/mInputText.spec.ts
+++ src/components/__tests__/mInputText.spec.ts
@@ -7,16 +7,29 @@ import { render, screen } from '@testing-library/vue'
 
 describe('Input Text', () => {
     it('deve aceitar um valor definido pelo usuário', async () => {
-        render(MInputText)
+
+        const { emitted } = render(MInputText);
+
         const user = userEvent.setup()
 
         const input = screen.getByRole('input-text') as HTMLInputElement
         input.focus()
         await user.keyboard('aaa')
 
-        expect(input.value).toBe('aaa')
+        expect(emitted('update:modelValue').length).toBe(3);
+
     })
-    // it('deve aceitar um valor definido programaticamente')
+    it('deve aceitar um valor definido programaticamente', async () => {
+        const { getByRole } = render(MInputText, {
+            props: {
+                modelValue: 'texto de teste'
+            }
+        });
+
+        const input = getByRole("input-text") as HTMLInputElement;
+
+        expect(input.value).toBe('texto de teste');
+    })
     // it('deve emitir o evento change quando o seu valor for alterado pelo usuário')
     // it('deve emitir o evento change quando o seu valor for alterado programticamente');
 })
